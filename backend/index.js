de068845101354704bdaf7405e7c9c90b997aaf9
sedian27 <diestrasergio@gmail.com>
2022-02-15 10:03:25 -0500

    add role in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import petRoutes from "./routes/petRoutes.js";
+import roleRoutes from "./routes/roleRoutes.js";
 import db from "./db/db.js";
 
 dotenv.config();
@@ -11,8 +12,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/user", userRoutes);
 app.use("/api/pet", petRoutes);
+app.use("/api/role", roleRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("NODE SERVER RUNNING IN PORT: ", process.env.PORT);
